341e08288a03488e2c771b58adbe0583a44822f1
22010621 <q.wang0220222@arts.ac.uk>
2024-12-06 19:22:40 +0800

    Filter games by genre

--- src/components/GenreSkeleton.tsx
+++ src/components/GenreSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton, VStack } from "@chakra-ui/react";
 
 const GenreSkeleton = () => {
   return (
-    <VStack paddingY={5}>
+    <VStack paddingY={5} borderRadius={10}>
       <Skeleton height="30px" width="150px" />
       <Skeleton height="10px" width="150px" />
     </VStack>
