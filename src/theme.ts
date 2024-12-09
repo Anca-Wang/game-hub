65724c22da95aaafcbdf54bfb6189b1cfd31f12e
22010621 <q.wang0220222@arts.ac.uk>
2024-12-09 23:12:16 +0800

    Customize colors to get darker greys

--- src/theme.ts
+++ src/theme.ts
@@ -4,7 +4,20 @@ const customTheme = extendTheme({
   config: {
     initialColorMode: "dark", // Set the initial color mode to dark
     useSystemColorMode: false, // Disable system color mode to always start with the specified color mode
-  },
+  }, colors:{
+    gray: {
+      50: '#f9f9f9',
+      100: '#ededed',
+      200: '#d3d3d3',
+      300: '#b3b3b3',
+      400: '#a0a0a0',
+      500: '#898989',
+      600: '#6c6c6c',
+      700: '#202020',
+      800: '#121212',
+      900: '#111'
+    }
+  }
 });
 
 export default customTheme
