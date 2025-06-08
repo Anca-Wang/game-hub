ac881ce679568f7bb0713c67706a667612d94302
Qijing <ancawang0@gmail.com>
2025-06-08 19:05:52 +0100

    Refactor: Create lookup hooks to simplify genre and platform lookup

--- src/components/GameHeading.tsx
+++ src/components/GameHeading.tsx
@@ -1,7 +1,7 @@
 import { GameQuery } from "@/App";
 import { Heading } from "@chakra-ui/react";
-import useGenres from "@/hooks/useGenres";
-import usePlatforms from "@/hooks/usePlatforms";
+import usePlatform from "@/hooks/usePlatform";
+import useGenre from "@/hooks/useGenre";
 
 interface Props {
   gameQuery: GameQuery;
@@ -9,11 +9,9 @@ interface Props {
 
 const GameHeading = ({ gameQuery }: Props) => {
 
-  const {data: genres} = useGenres()
-  const genre = genres?.find(g => g.id === gameQuery.genreId)
+  const genre = useGenre(gameQuery.genreId)
 
-  const {data: platforms} = usePlatforms()
-  const platform = platforms?.find(p => p.id === gameQuery.platformId)
+  const platform = usePlatform(gameQuery.platformId)
 
   const heading = `${platform?.name || ""} ${
     genre?.name || ""
